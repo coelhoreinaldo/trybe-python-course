b2dd622bb0e2127f2a684339f5ccf2b0bb76be4f
coelhoreinaldo <scoelhor7@gmail.com>
2024-09-11 16:01:27 -0300

    test: mocking with faker

--- 3-tests/faker_names.py
+++ 3-tests/faker_names.py
@@ -1,11 +1,11 @@
 from faker import Faker
 
 
-faker = Faker()  # Adding seed to persist the results
+faker = Faker(locale="pt_BR")
 
-Faker.seed(0)
+Faker.seed(0)  # Adding seed to persist the results
+
 
 print(faker.name())
-print(faker.name())
-print(faker.name())
-print(faker.name())
+print(faker.phone_number())
+print(faker.cpf())
