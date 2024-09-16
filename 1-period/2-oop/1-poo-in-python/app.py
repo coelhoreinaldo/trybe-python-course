9bc06ec68760ba47e9f91c37ac01ef3ac330535c
coelhoreinaldo <scoelhor7@gmail.com>
2024-09-16 10:26:18 -0300

    feat: exs

--- 1-period/2-oop/1-poo-in-python/app.py
+++ 1-period/2-oop/1-poo-in-python/app.py
@@ -15,3 +15,20 @@ class Database:
 
 database_1 = Database()
 database_2 = Database()
+
+
+class Car:
+    def __init__(self, brand: str, model: str, year: int, color: str):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.color = color
+
+    def identify(self):
+        print(
+            f"Meu carro é um {self.model}, da {self.brand}. Ele é do ano {self.year} e tem a cor {self.color}"
+        )
+
+
+car_1 = Car("Chevrolet", "Onix", 2020, "Preto")
+car_1.identify()
