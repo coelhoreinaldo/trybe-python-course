ecb5b341f99297c43b31f94266d03f04c867f194
coelhoreinaldo <scoelhor7@gmail.com>
2024-09-16 12:21:49 -0300

    feat: ex 1

---
+++ 1-period/2-oop/1-poo-in-python/exercises.py
@@ -0,0 +1,9 @@
+# Exercício 1
+# Escreva uma função que recebe uma lista de inteiros e retorna a soma desses valores.
+
+
+def sum_integers(integers: list[int]) -> int:
+    return sum(integers)
+
+
+print(sum_integers([1, 2, 3, 4, 5]))
