b171f0a90d5232402cd642240b95e878a7ccbed2
coelhoreinaldo <scoelhor7@gmail.com>
2024-09-16 12:26:27 -0300

    feat: ex 2

--- 1-period/2-oop/1-poo-in-python/exercises.py
+++ 1-period/2-oop/1-poo-in-python/exercises.py
@@ -6,4 +6,17 @@ def sum_integers(integers: list[int]) -> int:
     return sum(integers)
 
 
-print(sum_integers([1, 2, 3, 4, 5]))
+sum_integers([1, 2, 3, 4, 5])
+
+# Exercício 2
+# Escreva uma função que recebe uma lista de strings e um caractere e retorna uma lista com as strings que começam com o caractere fornecido.
+
+
+def find_str(strings: list[str], char: str) -> list[str]:
+    return [s for s in strings if char in s]
+
+
+animals = ["cachorro", "gato", "elefante", "girafa"]
+char = "g"
+
+find_str(animals, char)
