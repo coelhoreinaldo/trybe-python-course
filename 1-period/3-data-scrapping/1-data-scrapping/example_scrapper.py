10127a6a039b255c915c5878c20f05a60f1e5565
coelhoreinaldo <scoelhor7@gmail.com>
2024-09-30 10:37:07 -0300

    python3

--- 1-period/3-data-scrapping/1-data-scrapping/example_scrapper.py
+++ 1-period/3-data-scrapping/1-data-scrapping/example_scrapper.py
@@ -2,17 +2,23 @@ from parsel import Selector
 import requests
 
 
-# Define a primeira página como próxima a ter seu conteúdo recuperado
 URL_BASE = "http://books.toscrape.com/catalogue/"
 next_page_url: str | None = "page-1.html"
 while next_page_url:
-    # Busca o conteúdo da próxima página
     response = requests.get(URL_BASE + next_page_url)
     selector = Selector(text=response.text)
-    # Imprime os produtos de uma determinada página
     for product in selector.css(".product_pod"):
-        title = product.css("h3 a::attr(title)").get()
-        price = product.css(".price_color::text").get()
-        print(title, price)
-    # Descobre qual é a próxima página
+        # title = product.css("h3 a::attr(title)").get()
+        # price = product.css(".price_color::text").get()
+        # print(title, price)
+
+        detail_href = product.css("h3 a::attr(href)").get() or ""
+        detail_page_url = URL_BASE + detail_href
+
+        detail_response = requests.get(detail_page_url)
+        detail_selector = Selector(text=detail_response.text)
+
+        description = detail_selector.css("#product_description ~ p::text").get()
+        print(description)
+
     next_page_url = selector.css(".next a::attr(href)").get()
