7f4007d4caab9ab0008b3fc9ff0d480e03fe4352
coelhoreinaldo <scoelhor7@gmail.com>
2024-10-08 19:09:24 -0300

    feat: add products app

--- 2-period/2-django-I/1-database-environments-and-orm/ecommerce/ecommerce/settings.py
+++ 2-period/2-django-I/1-database-environments-and-orm/ecommerce/ecommerce/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "products",
 ]
 
 MIDDLEWARE = [
@@ -75,14 +76,14 @@ WSGI_APPLICATION = "ecommerce.wsgi.application"
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ecommerce_database',
-        'USER': 'root',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        }
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "ecommerce_database",
+        "USER": "root",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+    }
 }
 
 # Password validation
