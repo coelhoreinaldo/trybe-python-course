288c60103e2c6dba2245b90cad82edcdc6c5ad6f
coelhoreinaldo <scoelhor7@gmail.com>
2024-10-08 22:59:06 -0300

    feat: create mirgations and dockerfile

--- 2-period/2-django-I/1-database-environments-and-orm/schedule/schedule/schedule/settings.py
+++ 2-period/2-django-I/1-database-environments-and-orm/schedule/schedule/schedule/settings.py
@@ -73,13 +73,17 @@ WSGI_APPLICATION = "schedule.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "schedule_database",
+        "USER": "root",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
